9c452b9d7711b8316d603ac50dc07cf797716f5b
ryanata <ataryan47@gmail.com>
2022-10-12 16:15:21 -0400

    Added page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Landing from './pages/Landing';
+import About from './pages/About';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Landing/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Welcome to password manager</h1>
+        <nav className='PageNavigation'>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="about" element={<About/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <>
+            <h1>About</h1>
+            <p>This is a password manager app</p>
+        </>
+     );
+}
+ 
+export default About;
