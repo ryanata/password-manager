8a4bc48850bbc9954c76dac71a327df073655dab
Camila Sinelli <camilasinelli@gmail.com>
2022-10-21 03:31:10 -0400

    Added Nav Bar component and favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Landing from './pages/Landing';
 import About from './pages/About';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
+import { LandingNav } from './components/LandingNav';
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
@@ -12,10 +13,12 @@ function App() {
           <nav className='PageNavigation'>
             <Link to="/">Home</Link>
             <Link to="about">About</Link>
+            <Link to="test">Test</Link>
           </nav>
           <Routes>
             <Route path="/" element={<Landing/>}/>
             <Route path="about" element={<About/>}></Route>
+            <Route path="test" element={<LandingNav/>}></Route>
           </Routes>
         </div>
       </BrowserRouter>
