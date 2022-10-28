08b14419bfb295d3a08c7e39dc2b64e4f7bc7f4e
Adam <leiadam74@gmail.com>
2022-10-28 15:25:16 -0400

    Changed Input Styled the button to more match figma

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,11 +1,30 @@
-import { Input, Group, Button } from '@mantine/core';
-import { IconAt, IconPlus } from '@tabler/icons';
+import { TextInput, Group, Button } from '@mantine/core';
+import { IconSearch, IconPlus } from '@tabler/icons';
 
 export function SearchBar() {
   return (
     <Group>
-        <Input icon={<IconAt />} placeholder="Search"/>
-        <Button uppercase leftIcon={<IconPlus size={28}/>}>add new</Button>
+        <TextInput 
+          icon={<IconSearch size={28}/>}
+          placeholder="Search"
+        />
+        <Button 
+          uppercase
+          leftIcon={<IconPlus size={28}/>}
+          styles={(theme) => ({
+            root: {
+              backgroundColor: '#4681D0',
+              fontSize:18,
+              paddingLeft:10,
+              paddingRight:10,
+              '&:hover': {
+                backgroundColor: theme.fn.darken('#4681D0', 0.05),
+              },
+            },
+          })}
+          >
+            add new
+        </Button>
     </Group>
   );
 }
