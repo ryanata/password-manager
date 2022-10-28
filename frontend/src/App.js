675c74b0fb487f50089dace15c9312e7bd83d185
ryanata <ataryan47@gmail.com>
2022-10-28 03:31:10 -0400

    Added api calls for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,39 @@ import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-                colorScheme: "light",
-                fontFamily: "'Public Sans', sans-serif",
-                headings: {
-                    fontFamily: "'Nunito', sans-serif",
-                },
-            }}
-        >
-            <BrowserRouter>
-                <div className="App">
-                    <Routes>
-                        <Route path="/" element={<Landing />} />
-                        <Route path="dashboard" element={<Dashboard />}></Route>
-                        <Route path="*" element={<h1>404</h1>}></Route>
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </MantineProvider>
+        <QueryClientProvider client={queryClient}>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    colorScheme: "light",
+                    colors: {
+                        "steel-blue": { 6: "#4681D0" },
+                    },
+                    fontFamily: "'Public Sans', sans-serif",
+                    headings: {
+                        fontFamily: "'Nunito', sans-serif",
+                    },
+                }}
+            >
+                <BrowserRouter>
+                    <div className="App">
+                        <Routes>
+                            <Route path="/" element={<Landing />} />
+                            <Route path="dashboard" element={<Dashboard />}></Route>
+                            <Route path="*" element={<h1>404</h1>}></Route>
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </MantineProvider>
+        </QueryClientProvider>
     );
 }
 
