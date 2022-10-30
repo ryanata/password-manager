271e305bb72447178e8337fe0e74a1c2a3583ee0
ryanata <ataryan47@gmail.com>
2022-10-30 18:40:21 -0400

    Updated dir routes

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use('/api/verification', require('./routes/verificationRoutes'));
 // Heroku deployment
 if (process.env.NODE_ENV === 'production')
 {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('../frontend/build'));
     app.get('*', (req, res) =>
     {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
     });
 }
 
