4e4567a5f3ab6137472f8d0bb16dcc3f3afd41a1
ryanata <ataryan47@gmail.com>
2022-10-29 23:24:04 -0400

    Integrated dashboard with login modal

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { Center, Loader, Text, Anchor } from "@mantine/core";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 const Dashboard = () => {
+    let token = "none";
+    try {
+        token = JSON.parse(localStorage.getItem("pwdlyToken"));
+    } catch (error) {
+        console.log(error);
+    }
+    const { data, isLoading, error } = useQuery(["getUser"], () =>
+        axios.get("/api/user/me", { headers: { Authorization: `Bearer ${token}` } })
+    );
+
+    if (isLoading) {
+        return (
+            <Center style={{ width: "100%", height: "100vh" }}>
+                <Loader size="xl" color="steel-blue" />
+            </Center>
+        );
+    }
+
+    if (error) {
+        return (
+            <Center style={{ width: "100%", height: "100vh" }}>
+                <Text>
+                    Your session has expired. Please log in on our
+                    <Anchor href="/"> home page.</Anchor>
+                </Text>
+            </Center>
+        );
+    }
+
+    const user = data.data;
     return (
         <div>
-            <p>Hi!</p>
+            <h1>Dashboard</h1>
+            <p>{"Welcome" + " " + user.name.firstName + " " + user.name.lastName}</p>
         </div>
     );
 };
