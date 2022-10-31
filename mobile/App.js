99469a8beeb1e99b32e5e7f9e1c309ecf39f76ef
ryanata <ataryan47@gmail.com>
2022-10-31 17:21:04 -0400

    Added expo react native app

---
+++ mobile/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator intialRouteName="Login" screenOptions={
+        {
+          headerShown: false
+        }
+      }>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ mobile/pages/Login.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Button, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+const Login = ({ navigation }) => {
+    return ( 
+        <View style={styles.container}>
+            <Text>Login</Text>
+            <Button
+                title="Don't have an account? Sign Up"
+                onPress={() => navigation.navigate('Signup')}
+            />
+        </View>
+     );
+}
+ 
+export default Login;
