70083df4920a459dac81692eb643e4d0c06abd6a
Nar407 <guidalyj@gmail.com>
2022-11-01 02:01:08 -0400

    Update formatting

--- frontend/src/components/ProductDescription.js
+++ frontend/src/components/ProductDescription.js
@@ -11,12 +11,13 @@ export function ProductDescription() {
     <>
     
     <Grid grow gutter={40}>
-        <Grid.Col span={4}>
-            <Title sx={{ color: '#ffffff', fontSize: 60, lineHeight: 1.4, float: 'left', textAlign: 'left', paddingLeft: 30 }}>
+        <Grid.Col span={2}>
+            <Title sx={{ color: '#ffffff', fontSize: 60, lineHeight: 1.4, float: 'left', fontFamily: 'nunito', textAlign: 'left', paddingLeft: 50 }}>
                 Take your passwords everywhere, safe.
             </Title>
-            <Text sx={{ color: '#ffffff', fontSize: 26, lineHeight: 1.4, float: 'left',fontFamily: 'public sans', textAlign: 'left', paddingLeft: 30 }}>
-                {"The only password manager you need for all your life chapters. Learn how you can store, share, and secure your life with pwdly."}
+            <Text sx={{ color: '#ffffff', fontSize: 26, lineHeight: 1.4,float: 'left',fontFamily: 'public sans', textAlign: 'left', paddingLeft: 50 }}>
+                The only password manager you need for all your life chapters. 
+                Learn how you can store, share, and secure your life with <Text color="red" inherit component="span">pwdly</Text>.
             </Text>
             <Button sx={{color: '#ffffff', backgroundColor: '#052754', marginRight: 12, marginTop: 20 ,fontFamily: 'nunito', fontSize: 30}} radius="xl">
                 Learn More
@@ -27,8 +28,8 @@ export function ProductDescription() {
         </Grid.Col>
       <Grid.Col span={4}>
       <Image
-        width={690}
-        height={490}
+        width={990}
+        height={590}
         radius="md"
         src="https://cdn.dribbble.com/users/1930239/screenshots/18753108/media/23933d022522e7fb8abdd23996b6f037.png"
         alt="Random unsplash image"
