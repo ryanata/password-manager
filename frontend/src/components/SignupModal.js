0133ffb6bdc0ce3579c53886292d8f634a9d583a
andreadonoso <98114065+andreadonoso@users.noreply.github.com>
2022-11-02 20:35:58 -0400

    signup works

--- frontend/src/components/SignupModal.js
+++ frontend/src/components/SignupModal.js
@@ -12,8 +12,10 @@ import {
     TextInput,
 } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
-import { useForm } from "@mantine/form";
-import { useState } from "react";
+import { useForm } from '@mantine/form';
+import { useState } from 'react';
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
     link: {
@@ -32,9 +34,64 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
+
+
 const SignupModal = ({ opened, closed }) => {
+    // Styling
     const { classes, theme } = useStyles();
+
+    // Hooks
     const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm - 1}px)`);
+    const [submittedValues, setSubmittedValues] = useState('');
+    const [alert, setAlert] = useState('');
+    
+    // Form close
+    const onClose = () => {
+        // Triggers css display: none
+        closed();
+    };
+
+    const form = useForm({
+        initialValues: {
+            firstName:"",
+            lastName:"",
+            email: "",
+            password: "",
+            loggedIn: false
+        },
+        validate: {
+            email: (value) => !value.includes("@") && "Invalid email",
+        },
+    });
+
+    const formHandler = (values) => {
+        axios
+        .post("/api/user/register", {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            phoneNumber: 123,
+            email: values.email,
+            password: values.password,
+        })
+        .then((res) => {
+            // If login successful
+            if (res.status === 201) {
+                // Redirect to dashboard
+                localStorage.setItem("pwdlyToken", JSON.stringify(res.data.user.token));
+                form.setFieldValue("loggedIn", true);
+            }
+        })
+        .catch((err) => {
+            // Set alert message
+            setAlert(err.response.data.message);
+        
+        })
+
+    }
+    if (form.values.loggedIn) {
+        return <Navigate to="/dashboard" />;
+    }
+
     return (
         <Modal
             centered={true}
@@ -57,13 +114,17 @@ const SignupModal = ({ opened, closed }) => {
                 {"Already have an account? "}
                 <Anchor>Log in</Anchor>
             </Text>
-            <form>
+            <form
+            onSubmit={form.onSubmit(formHandler)}
+            >
                 <Stack>
                     <Group position="center" spacing="sm" grow>
                         <TextInput
                             required={true}
                             label="First name"
                             placeholder="john"
+                            value={form.values.firstName}
+                            {...form.getInputProps('firstName')}
                             // value={form.values.email}
                             // onChange={(event) => form.setFieldValue("email", event.currentTarget.value)}
                             // error={form.errors.email && "Invalid email"}
@@ -72,6 +133,9 @@ const SignupModal = ({ opened, closed }) => {
                             required={true}
                             label="Last name"
                             placeholder="doe"
+                            mt="md"
+                            value={form.values.lastName}
+                            {...form.getInputProps('lastName')}
                             // value={form.values.email}
                             // onChange={(event) => form.setFieldValue("email", event.currentTarget.value)}
                             // error={form.errors.email && "Invalid email"}
@@ -81,6 +145,9 @@ const SignupModal = ({ opened, closed }) => {
                         required={true}
                         label="Email"
                         placeholder="john.doe@gmail.com"
+                        mt="md"
+                        value={form.values.email}
+                        {...form.getInputProps('email')}
                         // value={form.values.email}
                         // onChange={(event) => form.setFieldValue("email", event.currentTarget.value)}
                         // error={form.errors.email && "Invalid email"}
@@ -90,18 +157,23 @@ const SignupModal = ({ opened, closed }) => {
                         required={true}
                         label="Password"
                         placeholder="********"
-                        // value={form.values.password}
-                        // onChange={(event) => form.setFieldValue("password", event.currentTarget.value)}
+                        mt="md"
+                        value={form.values.password}
+                        {...form.getInputProps('password')}
                     />
 
                     <Button
                         type="submit"
-                        // onClick={() => form.setFieldValue("submittingLogin", true)}
+                        onClick={() => form.setFieldValue("submittingSignup", true)}
                         size="md"
                         mb="sm"
+                        mt="md"
                     >
                         Sign up
                     </Button>
+                    {alert && <Text color="red"> {alert}
+                    
+                    </Text>}
                 </Stack>
             </form>
         </Modal>
