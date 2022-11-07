b3ada25bfceff5a53ace09195424a5d6b8f887f9
Adam <leiadam74@gmail.com>
2022-11-07 00:51:45 -0500

    Added styles to match sign in

--- mobile/pages/Register.js
+++ mobile/pages/Register.js
@@ -1,14 +1,5 @@
 import { StyleSheet, Button, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
-
 const Register = ({ navigation }) => {
     return (
         <View style={styles.container}>
@@ -20,5 +11,57 @@ const Register = ({ navigation }) => {
         </View>
      );
 }
- 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    image :{
+        marginBottom: 40,
+        alignItems: "center",
+    },
+
+    inputView: {
+        backgroundColor: "#ffffff",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        alignItems: "center",
+    },
+    
+    SignBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#ffffff",
+    },
+    errorMessageContainerStyle: {
+        marginBottom: 8,
+        backgroundColor: '#fee8e6',
+        padding: 8,
+        borderRadius: 4,
+    },
+
+    errorMessageTextStyle: {
+        color: '#db2828',
+        textAlign: 'center',
+        fontSize: 12,
+    },
+});
+
 export default Register;
