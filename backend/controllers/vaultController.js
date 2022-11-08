c34e1ac568102ea5f651427e42c76df3e8ee0406
Billy Furie <furie@knights.ucf.edu>
2022-11-08 12:39:38 -0500

    Created updateVault API which can be used to update a vaults name, masterPassword, or phoneNumber

--- backend/controllers/vaultController.js
+++ backend/controllers/vaultController.js
@@ -65,10 +65,11 @@ const createVault = asyncHandler(async (req, res) => {
 
 // @desc    Update vault
 // @route   PUT /api/vault/{vaultID}
+// can be used to update name, masterPassword, or phone
 const updateVault = asyncHandler(async (req, res) => {
     const vaultID = req.params.vaultID;
-    const { vaultName, masterPassword, phoneNumber } = req.body;
-    let vaultNameUpdated = false;
+    const { name, masterPassword, phone } = req.body;
+    let update = {}
 
     const vaultExists = await Vault.findById(vaultID);
 
@@ -77,72 +78,34 @@ const updateVault = asyncHandler(async (req, res) => {
         throw new Error('This vault does not exist');
     }
 
-    if (!vaultName && !masterPassword && !phoneNumber) {
+    if (!name && !masterPassword && !phone) {
         res.status(400);
         throw new Error('Please enter a field to update');
     }
 
-    if (vaultName) {
-        const vault = await Vault.findByIdAndUpdate(vaultID, { name: vaultName });
-
-        if (vault) {
-            vaultNameUpdated = true;
-        }
-        else {
-            res.status(400);
-            throw new Error('Vault name could not be updated');
-        }
+    // gather all the fields in which we want to update
+    if (name) {
+        update["name"] = name;
     }
 
-    res.status(200);
-    res.send(vaultNameUpdated);
+    if (masterPassword) {
+        update["masterPassword"] = masterPassword;
+    }
 
-    // // Check for user with this userID
-    // const userExists = await User.findOne({ userID });
-    // if (!userExists) {
-    //     res.status(400);
-    //     throw new Error('User does not exist');
-    // }
+    if (phone) {
+        update["mfa.phone"] = phone;
+    }
 
-    // // // Hash password
-    // // const salt = await bcrypt.genSalt(10)
-    // // const hashedPassword = await bcrypt.hash(password, salt)
+    const vault = await Vault.findByIdAndUpdate(vaultID, update, {new: true});
 
-    // const vault = await Vault.create({
-    //     name: vaultName,
-    //     masterPassword: masterPassword,
-    //     mfa:  {
-    //         phone: phoneNumber
-    //     },
-    //     userID: userID
-    // });
-
-    // if (vault) {
-    //     const user = await User.findOneAndUpdate(
-    //         { _id: userID }, 
-    //         { $push: { 
-    //             vaults: vault
-    //         } 
-    //     });
-
-    //     if (!user) {
-    //         res.status(400);
-    //         throw new Error('Vault could not be added to user');
-    //     }
-
-    //     res.status(201).json({
-    //         message: 'Vault created!',
-    //         vault: {
-    //             name: vault.name,
-    //             masterPassword: vault.masterPassword,
-    //             mfa: vault.mfa,
-    //             userID: vault.userID
-    //         },
-    //     });
-    // } else {
-    //     res.status(400);
-    //     throw new Error('Vault was not created');
-    // }
+    if (vault) {
+        res.status(200);
+        res.send(vault);
+    }
+    else {
+        res.status(400);
+        throw new Error('Vault could not be updated');
+    }
 });
 
 module.exports = {
