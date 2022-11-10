aa8a0b8207538bfe427cd8a441b9d9b209e2149b
andreadonoso <98114065+andreadonoso@users.noreply.github.com>
2022-11-09 23:51:49 -0500

    (finished?) fixed color and display

--- frontend/src/components/DashboardHeader.js
+++ frontend/src/components/DashboardHeader.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { IconUser } from "@tabler/icons";
+import { default as iconUser } from "../assets/iconUser.png";
 import { default as Logo } from "../assets/logo.svg";
-import { Anchor, Group, Header, createStyles, Text } from "@mantine/core";
+import { Anchor, Group, Header, createStyles, Text, Space, Title, Image } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-// FIX color 4681D0 & padding, 
 const useStyles = createStyles((theme) => ({
     header: {
-        // padding: theme.spacing.md,
-        // paddingTop: 0,
-        // marginLeft: -theme.spacing.xs,
-        // paddingLeft: theme.spacing.lg,
-        // marginRight: -theme.spacing.md,
-        backgroundColor: theme.colors.blue[5],
+        backgroundColor: "#4681D0",
         color: theme.white,
         borderBottom: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
     },
 }));
-
-// Add "account" button with icon and redirect
 
 export default function DashboardHeader() {
     let token = "none";
@@ -33,24 +25,28 @@ export default function DashboardHeader() {
     );
 
     const user = data.data;
-
     const { classes } = useStyles();
 
     return (
         <Header height={60} p="xs" className={classes.header}>
             {
                 <Group position="apart">
-                    <img src={Logo} alt="pwdly logo" />
-                    <Group position="apart">
-                        <Text>{`Welcome ${user.name.firstName} ${user.name.lastName}!`}</Text>
+                    <Group>
+                        <Space h="md" />
+                        <img src={Logo} alt="pwdly logo" />
+                    </Group>
+                    <Group position="apart" ta="center">
+                        <Title order={3} weight={300} transform="capitalize">{`${user.name.firstName} ${user.name.lastName[0]}`}</Title>
+                        <Space h="s" />
                         <Anchor
                             onClick={() => {
                                 localStorage.removeItem("pwdlyToken");
                             }}
                             href="/"
                         >
-                            Log out
+                            <Image height={20} fit="contain" src={iconUser} />
                         </Anchor>
+                        <Space h="md" />
                     </Group>
                 </Group>
             }
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -67,9 +67,7 @@ const Dashboard = () => {
                         {/* Navbar content */}
                     </Navbar>
                 }
-                header={
-                    <DashboardHeader />
-                }
+                header={<DashboardHeader />}
                 styles={(theme) => ({
                     main: {
                         backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
