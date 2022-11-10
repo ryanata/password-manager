75df3ecddca254f719652eb002b662d433c09fc3
andreadonoso <98114065+andreadonoso@users.noreply.github.com>
2022-11-09 20:24:42 -0500

    styling added (needs work)

--- frontend/src/components/DashboardHeader.js
+++ frontend/src/components/DashboardHeader.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { IconUser } from "@tabler/icons";
 import { default as Logo } from "../assets/logo.svg";
-import { Anchor, AppShell, Center, Group, Header, Loader, Navbar, Text } from "@mantine/core";
+import { Anchor, Group, Header, createStyles, Text } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+
+// FIX color 4681D0 & padding, 
+const useStyles = createStyles((theme) => ({
+    header: {
+        // padding: theme.spacing.md,
+        // paddingTop: 0,
+        // marginLeft: -theme.spacing.xs,
+        // paddingLeft: theme.spacing.lg,
+        // marginRight: -theme.spacing.md,
+        backgroundColor: theme.colors.blue[5],
+        color: theme.white,
+        borderBottom: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
+    },
+}));
+
+// Add "account" button with icon and redirect
 
 export default function DashboardHeader() {
     let token = "none";
@@ -17,25 +33,27 @@ export default function DashboardHeader() {
     );
 
     const user = data.data;
-  return (
-    <Header height={60} p="xs" color='steel-blue'>
-        {
-            <Group position="apart">
-                <img src={Logo} alt="pwdly logo" />
+
+    const { classes } = useStyles();
+
+    return (
+        <Header height={60} p="xs" className={classes.header}>
+            {
                 <Group position="apart">
-                    <Text>{`Welcome ${user.name.firstName} ${user.name.lastName}!`}</Text>
-                    <Anchor
-                        onClick={() => {
-                            localStorage.removeItem("pwdlyToken");
-                        }}
-                        href="/"
-                    >
-                        Log out
-                    </Anchor>
+                    <img src={Logo} alt="pwdly logo" />
+                    <Group position="apart">
+                        <Text>{`Welcome ${user.name.firstName} ${user.name.lastName}!`}</Text>
+                        <Anchor
+                            onClick={() => {
+                                localStorage.removeItem("pwdlyToken");
+                            }}
+                            href="/"
+                        >
+                            Log out
+                        </Anchor>
+                    </Group>
                 </Group>
-            </Group>
-        }
-    </Header>
-    
-  )
+            }
+        </Header>
+    );
 }
