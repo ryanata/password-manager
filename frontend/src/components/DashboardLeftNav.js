bed284a3711239de1139a13916a7cf662fdc1d16
Camila Sinelli <camilasinelli@gmail.com>
2022-11-10 00:09:04 -0500

    Added Left Nav

--- frontend/src/components/DashboardLeftNav.js
+++ frontend/src/components/DashboardLeftNav.js
@@ -1,46 +1,34 @@
 import { Code, Group, Navbar, ScrollArea, createStyles } from "@mantine/core";
 import {
-    IconAdjustments,
-    IconCalendarStats,
-    IconFileAnalytics,
     IconGauge,
     IconLock,
-    IconNotes,
-    IconPresentationAnalytics,
+    IconSettings,
+    IconShieldLock,
+    IconUser,
 } from "@tabler/icons";
 
-import { LinksGroup } from "../NavbarLinksGroup/NavbarLinksGroup";
-import { UserButton } from "../UserButton/UserButton";
-import { Logo } from "./Logo";
+import { LinksGroup } from "./NavbarLinksGroups";
+import { UserButton } from "./UserButton";
+import { default as Logo } from "../assets/logo.svg";
 
 const mockdata = [
     { label: "Dashboard", icon: IconGauge },
     {
-        label: "Market news",
-        icon: IconNotes,
+        label: "Vaults",
+        icon: IconShieldLock,
         initiallyOpened: true,
         links: [
-            { label: "Overview", link: "/" },
-            { label: "Forecasts", link: "/" },
-            { label: "Outlook", link: "/" },
-            { label: "Real time", link: "/" },
+            { label: "Personal", link: "/" },
+            { label: "School", link: "/" },
+            { label: "Club", link: "/" },
+            { label: "Job", link: "/" },
         ],
     },
+    { label: "Password Generator", icon: IconLock },
+    { label: "Settings", icon: IconSettings },
     {
-        label: "Releases",
-        icon: IconCalendarStats,
-        links: [
-            { label: "Upcoming releases", link: "/" },
-            { label: "Previous releases", link: "/" },
-            { label: "Releases schedule", link: "/" },
-        ],
-    },
-    { label: "Analytics", icon: IconPresentationAnalytics },
-    { label: "Contracts", icon: IconFileAnalytics },
-    { label: "Settings", icon: IconAdjustments },
-    {
-        label: "Security",
-        icon: IconLock,
+        label: "Account",
+        icon: IconUser,
         links: [
             { label: "Enable 2FA", link: "/" },
             { label: "Change password", link: "/" },
@@ -51,7 +39,7 @@ const mockdata = [
 
 const useStyles = createStyles((theme) => ({
     navbar: {
-        backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+        backgroundColor: "#454545",
         paddingBottom: 0,
     },
 
@@ -81,29 +69,14 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
-export function NavbarNested() {
+export function DashboardLeftNav() {
     const { classes } = useStyles();
     const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
 
     return (
-        <Navbar height={800} width={{ sm: 300 }} p="md" className={classes.navbar}>
-            <Navbar.Section className={classes.header}>
-                <Group position="apart">
-                    <Logo width={120} />
-                    <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
-                </Group>
-            </Navbar.Section>
-
+        <Navbar height={800} width={{base:250}} p="md" className={classes.navbar}>
             <Navbar.Section grow className={classes.links} component={ScrollArea}>
                 <div className={classes.linksInner}>{links}</div>
-            </Navbar.Section>
-
-            <Navbar.Section className={classes.footer}>
-                <UserButton
-                    image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
-                    name="Ann Nullpointer"
-                    email="anullpointer@yahoo.com"
-                />
             </Navbar.Section>
         </Navbar>
     );
