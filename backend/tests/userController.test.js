a54557fb8b6dcdc67fe72d413ea00e2b9cc81751
Adam <leiadam74@gmail.com>
2022-12-05 17:51:38 -0500

    Added more test cases

--- backend/tests/userController.test.js
+++ backend/tests/userController.test.js
@@ -1,8 +1,9 @@
+const { ObjectID } = require("bson");
 const request = require("supertest");
 const { app } = require("../testServer");
 
 describe("API Tests", () => {
-    test('login on the test user', async () => {
+    it('should login on the test user', async () => {
         const data = {
             email: "nap@gmail.com",
             password: "123"
@@ -11,12 +12,47 @@ describe("API Tests", () => {
         expect(res.status).toBe(200); 
     });
 
-    test('login on the test user', async () => {
+    test('register on the existing user', async () => {
         const data = {
-            email: "nap@gmail.com",
+            firstName:"Ada",
+            lastName:"adA",
+            email: "654896@gmail.com",
+            phoneNumber: "1234567890",
             password: "123"
         };
-        const res = await request(app).post('/api/user/login').send(data).expect(200);
-        expect(res.status).toBe(200); 
+        const res = await request(app).post('/api/user/register').send(data);
+        expect(res.status).toBe(400); 
+    });
+
+    test('register with empty fields', async () => {
+        const data = {
+            firstName:"Ada",
+            lastName:"",
+            email: "654896@gmail.com",
+            phoneNumber: "1234567890",
+            password: ""
+        };
+        const res = await request(app).post('/api/user/register').send(data);
+        expect(res.status).toBe(400); 
+    });
+
+    test('addVault', async () => {
+        const data = {
+            name: 'asdfth',
+            masterPassword: "dskafhj",
+            userId: ObjectID('63596b74e50f22fe96391b94')
+        };
+        const res = await request(app).post('/api/vault').send(data);
+        expect(res.status).toBe(201); 
+    });
+
+    test('addVault with empty fields', async () => {
+        const data = {
+            name: '',
+            masterPassword: "",
+            userId: ObjectID('63596b74e50f22fe96391b94')
+        };
+        const res = await request(app).post('/api/vault').send(data);
+        expect(res.status).toBe(400); 
     });
 });
